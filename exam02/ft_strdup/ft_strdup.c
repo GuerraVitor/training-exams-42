6cc852e7f2dfbcf015cdd1f0455feaa40057fac0
GuerraVt <vitorhugoguerraguimaraes@gmail.com>
2026-03-01 20:06:00 -0300

    adding exercises from pratice

---
+++ exam02/ft_strdup/ft_strdup.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+char	*ft_strdup(char *src){
+	int i;
+	char *dest;
+
+	i = 0;
+	while(src[i] != '\0')
+		i++;
+	dest = malloc(sizeof (char) * (i + 1));
+	if (dest == NULL)
+		return NULL;
+	i = 0;
+	while(src[i] != '\0'){
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
